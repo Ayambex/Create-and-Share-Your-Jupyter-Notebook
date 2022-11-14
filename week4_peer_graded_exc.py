c4ffc28247734c11b86e85b4e7cc4ea5a32239fd
Ann <94022474+Ayambex@users.noreply.github.com>
2022-11-14 01:30:13 -0600

    Create week4_peer_graded_exc.py

---
+++ week4_peer_graded_exc.py
@@ -0,0 +1,21 @@
+My Jupyter Notebook on IBM Watson Studio
+Hannah Ayamba
+Data Science
+
+I am interested in data science because I want to use marchine learning algorithms to train models using data.
+
+The code below is to assign a variable and also add that varriable to an interger
+n = 8
+print (n + 20)
+28
+Fried rice can be eaten in the following gatherings
+At parties
+Birthdays
+Graaduation
+Naming ceremonies
+At weddings
+At Carnivals
+Here is a link to my Github account
+https://github.com/Ayambex/datasciencecoursera
+
+ 
